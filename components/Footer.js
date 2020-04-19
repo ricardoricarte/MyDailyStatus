ce7d5cd0f07987ad782652799f859add0ec0fe93
Ricardo Ricarte <56279938+ricardoricarte@users.noreply.github.com>
2020-04-18 23:10:44 -0300

    Update and rename components/Footer.js to  components/Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='py-4 text-center bg-gray-400 '>
             My Daily Status é um projeto criado durante o Fullstack Lab do DevPleno. <br />
-            Implementado por: <a href="https://www.linkedin.com/in/josé-vieira-s-junior/">José Junior</a>
+            Implementado por: <a href="https://https://www.linkedin.com/in/jose-ricardo-silva-082837191/">Ricardo Ricarte</a>
         </div>
     )
 }
